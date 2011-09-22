305b9ea0f17de930f728933e8226448c49fa6e8c
Phil <shxiaole@gmail.com>
2011-09-22 13:52:51 +0800

    ''

--- init.el
+++ init.el
@@ -38,7 +38,36 @@
 (add-to-list 'load-path "C:/Documents and Settings/Administrator/Application Data/.emacs.d/golang" t)
 (require 'go-mode-load)
 
+(require 'org-publish)
+(setq org-publish-project-alist
+      '(("org-notes"
+         :base-directory "F:/kuaipan/blog/" 
+		 :base-extension "org"
+         :publishing-directory "F:/me/shxiao.github.com/"
+         :publishing-function org-publish-org-to-html
+         :headline-level 4 
+		 :section-numbers nil
+         :table-of-contents t 
+		 :drawers t 
+		 :creator-info nil
+         :recursive t :link-home "/"
+         :style "<link rel='stylesheet' href='/etc/main.css' />"
+		 :auto-sitemap t                ; Generate sitemap.org automagically...
+		 :sitemap-filename "sitemap.org"  ; ... call it sitemap.org (it's the default)
+		 :sitemap-title "Sitemap"  
+		 :org-export-html-postamble nil	)
+        ("org-static"
+         :base-directory "F:/kuaipan/blog"
+         :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
+         :publishing-directory "F:/me/shxiao.github.com/static"
+         :publishing-function org-publish-attachment
+         :recursive t)
+        ("org" :components ("org-notes" "org-static"))))
+
 ;;tab
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 4)
 (setq indent-line-function 'insert-tab)
+
+(setq default-buffer-file-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
